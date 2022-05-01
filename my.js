3f8f8d3ebea062a0d287172bb7eefcf83abd6c84
antiadamiani <tamar.chkoidze.1@mail.iliauni.edu.ge>
2022-05-01 18:21:08 +0400

    added select color's buletts, button.

--- my.js
+++ my.js
@@ -1,3 +1,4 @@
+// დაიწყო fourth section 
 let faqs = document.querySelectorAll('.faq .faqDiv');
 faqs.forEach(fa => {
     fa.onclick = () => {
@@ -6,7 +7,55 @@ faqs.forEach(fa => {
     }
 })
 
+// დაიწყო fifth section
+let userRecomendation = [
+    {
+        photo: "./image/airpodsBlack.png"
+    },
+    {
+        photo: "./image/airpodswhite.png"
+    },
+    {
+        photo: "./image/airpods red.png"
+    },
+    {
+        photo: "./image/airpods blue.jpg"
+    }
+]
 
 
+let currentIndex = 0;
+    // bullets();
+    setActiveBullets();
+    next();
+    
+    function next(){
+        let nextSlideIndex = currentIndex + 1;
+        
+        if(nextSlideIndex > userRecomendation.length - 1){
+            nextSlideIndex = 0;
+        }
+        setActiveSlide(nextSlideIndex);
+    };
+    
+    
+    function setActiveBullets(){
+        let bullet = document.getElementById('bullet');
+        let buttons = document.querySelectorAll('.color bullet');
+        for(let i = 0; i < buttons.length; i++){
+            buttons[i].classList.remove('active');
+            buttons[i].addEventListener('click', function(){
+                setActiveSlide(i);
+            });
+           
+        }
+        buttons[currentIndex].classList.add('active');
+    };
+    
+    function setActiveSlide(newIndex){
+        currentIndex = newIndex;
+        let image = document.querySelector('.airpods');
+        image.src = userRecomendation[currentIndex].photo;
 
-
+        setActiveBullets();
+    };
